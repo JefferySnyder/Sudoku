48f9c333f56c9c9de69c7ebe101bee11c8fc2848
JefferySnyder <jayasnyd@gmail.com>
2022-05-25 14:16:59 -0700

    rearranged format

--- solver.py
+++ solver.py
@@ -18,17 +18,14 @@ def solve(bo):
 
 
 def valid(bo, num, pos):
-    # Check row
     for i in range(len(bo[0])):
         if bo[pos[0]][i] == num and pos[1] != i:
             return False
 
-    # Check column
     for i in range(len(bo)):
         if bo[i][pos[1]] == num and pos[0] != i:
             return False
 
-    # Check box
     box_x = pos[1] // 3
     box_y = pos[0] // 3
 
